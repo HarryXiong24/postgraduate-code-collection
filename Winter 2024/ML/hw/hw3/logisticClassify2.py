fce47ba98b92061377f514d1ce03fb92f5a827c6
harryxiong24 <harryxiong24@gmail.com>
2024-02-05 21:54:00 -0800

    2024.2.5

--- Winter 2024/ML/hw/hw3/logisticClassify2.py
+++ Winter 2024/ML/hw/hw3/logisticClassify2.py
@@ -38,14 +38,19 @@ class logisticClassify2(ml.classifier):
 
     def plotBoundary(self,X,Y):
         """ Plot the (linear) decision boundary of the classifier, along with data """
-        if len(self.theta) != 3: raise ValueError('Data & model must be 2D');
-        ax = X.min(0),X.max(0); ax = (ax[0][0],ax[1][0],ax[0][1],ax[1][1]);
+        if len(self.theta) != 3: 
+            raise ValueError('Data & model must be 2D');
+        ax = X.min(0), X.max(0); 
+        ax = (ax[0][0],ax[1][0],ax[0][1],ax[1][1]);
         ## TODO: find points on decision boundary defined by theta0 + theta1 X1 + theta2 X2 == 0
         x1b = np.array([ax[0],ax[1]]);  # at X1 = points in x1b
-        x2b = NotImplementedError;      # TODO find x2 values as a function of x1's values
+        # theta0 + theta1*X1 + theta2*X2 = 0  =>  X2 = (-theta0 - theta1*X1)/theta2
+        x2b = (-self.theta[0]-self.theta[1]*x1b)/self.theta[2]   # TODO find x2 values as a function of x1's values
         ## Now plot the data and the resulting boundary:
         A = Y==self.classes[0]; # and plot it:
-        plt.plot(X[A,0],X[A,1],'b.',X[-A,0],X[-A,1],'r.',x1b,x2b,'k-'); plt.axis(ax); plt.draw();
+        plt.plot(X[A,0],X[A,1],'b.',X[~A,0],X[~A,1],'r.',x1b,x2b,'k-');
+        plt.axis(ax);
+        plt.draw();
 
     def predictSoft(self, X):
         """ Return the probability of each class under logistic regression """
@@ -58,10 +63,15 @@ class logisticClassify2(ml.classifier):
 
     def predict(self, X):
         """ Return the predictied class of each data point in X"""
-        raise NotImplementedError
         ## TODO: compute linear response r[i] = theta0 + theta1 X[i,1] + theta2 X[i,2] + ... for each i
+        Xconst = np.atleast_2d(X)
+        if Xconst.shape[1] == self.theta.shape[0] - 1:
+            Xconst = np.hstack([np.ones([Xconst.shape[0], 1]), Xconst])
+        
+        z = np.dot(Xconst, self.theta)
         ## TODO: if z[i] > 0, predict class 1:  Yhat[i] = self.classes[1]
         ##       else predict class 0:  Yhat[i] = self.classes[0]
+        Yhat = np.array([self.classes[1] if z[i] > 0 else self.classes[0] for i in range(len(z))])
         return Yhat
 
 
@@ -78,17 +88,18 @@ class logisticClassify2(ml.classifier):
             stepsize, epoch = initStep*2.0/(2.0+epoch), epoch+1; # update stepsize
             # Do an SGD pass through the entire data set:
             for i in np.random.permutation(M):
-                ri    = NotImplementedError;     # TODO: compute linear response r(x)
-                gradi = NotImplementedError;     # TODO: compute gradient of NLL loss
+                ri    = XX[i].dot(self.theta);     # TODO: compute linear response r(x)
+                sigmoid = 1./(1.+np.exp(-ri));
+                gradi = -(1-sigmoid) * XX[i,:] if YY[i] else sigmoid * XX[i,:];     # TODO: compute gradient of NLL loss
                 self.theta -= stepsize * gradi;  # take a gradient step
 
             J01.append( self.err(X,Y) )  # evaluate the current error rate
 
             ## TODO: compute surrogate loss (logistic negative log-likelihood)
             ##  Jsur = sum_i [ (log si) if yi==1 else (log(1-si)) ]
-            Jnll.append( NotImplementedError ) # TODO evaluate the current NLL loss
-            plt.figure(1); plt.plot(Jnll,'b-',J01,'r-'); plt.draw();    # plot losses
-            if N==2: plt.figure(2); self.plotBoundary(X,Y); plt.draw(); # & predictor if 2D
+            S = 1./(1.+np.exp(-(XX.dot(self.theta))))
+            Jsur = -np.mean(YY*np.log(S)+(1-YY)*np.log(1-S))
+            Jnll.append( Jsur ) # TODO evaluate the current NLL loss
             plt.pause(.01);                    # let OS draw the plot
 
             ## For debugging: you may want to print current parameters & losses
@@ -96,7 +107,9 @@ class logisticClassify2(ml.classifier):
             # raw_input()   # pause for keystroke
 
             # TODO check stopping criteria: exit if exceeded # of epochs ( > stopEpochs)
-            done = NotImplementedError;   # or if Jnll not changing between epochs ( < stopTol )
+            done = epoch>=stopEpochs or (epoch>1 and abs(Jnll[-1]-Jnll[-2])<stopTol);   # or if Jnll not changing between epochs ( < stopTol )
+        plt.figure(1); plt.plot(Jnll,'b-',J01,'r-'); plt.draw();    # plot losses
+        if N==2: plt.figure(2); self.plotBoundary(X,Y); plt.draw(); # & predictor if 2D
 
 
 ################################################################################
